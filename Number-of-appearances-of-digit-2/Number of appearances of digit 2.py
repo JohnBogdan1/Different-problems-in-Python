df79f5fe9685d7fe2c63f9afede3af6652ee181f
JohnPrime <bogdan_ionut.ion@cti.pub.ro>
2016-04-25 21:55:02 +0300

    Create Number of appearances of digit 2.py
    Count the number of appearances of digit 2 in all numbers that are between 0 and n.

---
+++ Number-of-appearances-of-digit-2/Number of appearances of digit 2.py
@@ -0,0 +1,14 @@
+def count(n, k):
+    while n > 0:
+        r = str(n).count('2')
+        k += r
+        n -= 1
+
+    return k
+
+
+if __name__ == '__main__':
+    number = 1250000
+
+    k = 0
+    print (count(number, k))
